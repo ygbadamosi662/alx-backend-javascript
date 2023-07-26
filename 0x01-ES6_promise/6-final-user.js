ddaa9fa5264044bdf93a7a04130f52384539a14a
ygbadamosi662 <gbadamosiy662@gmail.com>
2023-07-26 07:57:52 +0100

    okay

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,9 +4,8 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => (res.map((pr) => ({
-      ststus: pr.status,
+    .then((response) => (response.map((pr) => ({
+      status: pr.status,
       value: pr.status === 'fulfilled' ? pr.value : String(pr.reason),
     }))));
 }
-console.log(handleProfileSignup('diane', 'yusuf', 'me.txt'));
